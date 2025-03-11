34e4ca8b164b3c4528f4c3193fcb2af2a449a212
unknown <veronika.vavrochova@infis.cz>
2025-03-11 09:35:53 +0100

    dasdasdsasda

---
+++ main.py
@@ -0,0 +1,14 @@
+#vavrochová
+#cislo = int(input("Zadejte cislo"))
+if cislo % 2 == 0
+   print ("Cislo je sude")
+# else:
+ #   print("cislo je liche")
+ # name = input("Zadejte jmeno: ")
+# surname = input("Zadejte prijmeni: ")
+
+# if name == "jan" and surname == "Novak":
+     print("Ucitel anglictiny.")
+#elif:
+   print(f"Ahoj, [name] [surname]!")
+#Veronika.V
